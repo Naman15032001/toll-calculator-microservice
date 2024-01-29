9ca3355c6f0034e18aec236ee69a8ffe54f5f73f
Naman15032001 <namanjain15032001@gmail.com>
2024-01-29 11:49:01 +0530

    distance-cal-feature-added

--- distance_calculator/consumer.go
+++ distance_calculator/consumer.go
@@ -1,5 +1,63 @@
 package main
 
-type DataConsumer interface{
-	ConsumeData()
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Naman15032001/tolling/types"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/sirupsen/logrus"
+)
+
+// this can also be called kafka transport
+type KafkaConsumer struct {
+	consumer    *kafka.Consumer
+	isRunning   bool
+	calcService CalculatorServicer
+}
+
+func NewKafkaconsumer(topic string, svc CalculatorServicer) (*KafkaConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	c.SubscribeTopics([]string{topic}, nil)
+	return &KafkaConsumer{
+		consumer:    c,
+		calcService: svc,
+	}, nil
+}
+
+func (c *KafkaConsumer) readMessageLoop() {
+	for c.isRunning {
+		msg, err := c.consumer.ReadMessage(-1)
+
+		if err != nil {
+			logrus.Errorf("kafka consumer error %s", err)
+			continue
+		}
+		var data types.OBUDATA
+		if err := json.Unmarshal(msg.Value, &data); err != nil {
+			logrus.Errorf("JSON serialization error: %s", err)
+			continue
+		}
+		distance, err := c.calcService.CalculateDistance(data)
+		if err != nil {
+			logrus.Errorf("Service Calculation error: %s", err)
+			continue
+		}
+		fmt.Printf("distance %.2f\n", distance)
+	}
+}
+
+func (c *KafkaConsumer) Start() {
+	logrus.Info("kafka transport started")
+	c.isRunning = true
+	c.readMessageLoop()
 }
